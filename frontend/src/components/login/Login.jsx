5a5e53e599e1229cb567bebdae49072f14f8d2b4
creator-krixus <wilsonrueda2@gmail.com>
2021-11-12 23:48:19 -0500

    Change message in login

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Login() {
     return (
         <div className="col-auto text-center p-3">
-            <h1>Ingresa</h1>
+            <h1>Ingresa con tus credencialess</h1>
         </div>
     )
 }
